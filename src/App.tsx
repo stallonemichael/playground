b6f941c70d6cceea65f371a01252f914fc4153e9
Michael <mstallone@dakotasoft.com>
2025-02-18 18:24:15 -0500

    adds prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 const App = () => {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-100 to-blue-300 text-gray-800 flex flex-col items-center justify-center p-6">
-      <header className="text-center mb-4">
-        <h1 className="text-5xl font-bold">Mike's Sandbox</h1>
+    <div className='min-h-screen bg-gradient-to-br from-blue-100 to-blue-300 text-gray-800 flex flex-col items-center justify-center p-6'>
+      <header className='text-center mb-4'>
+        <h1 className='text-5xl font-bold'>Mike's Sandbox</h1>
       </header>
 
-      <div className="text-center w-full max-w-lg p-4">
-        <img 
-          src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExd3RhaXNveDh0Z2x0ODdncGMzMXAzMG5rZnVza2kwajhsbWFrNnBxbSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/9qqKRsdsLxXnMrMwM1/giphy.gif" 
-          alt="Rugrats" 
-          className="w-64 h-auto mx-auto rounded-lg"
+      <div className='text-center w-full max-w-lg p-4'>
+        <img
+          src='https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExd3RhaXNveDh0Z2x0ODdncGMzMXAzMG5rZnVza2kwajhsbWFrNnBxbSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/9qqKRsdsLxXnMrMwM1/giphy.gif'
+          alt='Rugrats'
+          className='w-64 h-auto mx-auto rounded-lg'
         />
       </div>
     </div>
